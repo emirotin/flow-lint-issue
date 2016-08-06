afdfe6699a0321d940d2b24de4db9704d8f70aa9
Eugene Mirotin <emirotin@gmail.com>
2016-08-06 10:52:37 +0300

    init

---
+++ index.js
@@ -0,0 +1,16 @@
+/* @flow */
+
+export { filter, reject } from 'lodash'
+
+type TArrayPredicate<T> = (el: T, i: number) => boolean
+type TArrayMap<T> = (el: T, i: number) => T
+
+export const updateWhere = <T> (list: Array<T>, pred: TArrayPredicate<T>, map: TArrayMap<T>): Array<T> =>
+  list.map((el, i) => (pred(el, i) ? map(el, i) : el))
+
+export const updateWhereField = <V, TObj: { [key: string]: V }> (
+    list: Array<TObj>, field: string, value: V, map: TArrayMap<TObj>
+): Array<TObj> => {
+  const pred = el => el[field] === value
+  return updateWhere(list, pred, map)
+}
